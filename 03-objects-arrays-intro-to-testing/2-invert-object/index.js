13f2857376d6de858b54cccffd4c782116e2ecb8
Damaskin Pavel <falard.damaskin@gmail.com>
2022-01-20 18:59:54 +0200

    Task 3.2 is ready

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -1,8 +1,20 @@
 /**
  * invertObj - should swap object keys and values
  * @param {object} obj - the initial object
- * @returns {object | undefined} - returns new object or undefined if nothing did't pass
+ * @returns {object | undefined} - returns new object or undefined if nothing didn't pass
  */
 export function invertObj(obj) {
+  if (!obj) {
+    return;
+  }
 
+  const res = {};
+
+  Object.entries(obj).forEach((item) => {
+    const [key, value] = item;
+
+    res[value] = key;
+  });
+
+  return res;
 }
