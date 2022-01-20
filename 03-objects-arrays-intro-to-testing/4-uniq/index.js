6c2fd4a98396347cd4c76ec24739c5976cded2ac
Damaskin Pavel <falard.damaskin@gmail.com>
2022-01-20 20:29:37 +0200

    Task 3.4 is ready

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,28 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (!arr) {
+    return [];
+  }
 
+  /* --- First Attempt --- */
+  /*const res = {};
+
+  arr.forEach((item) => {
+    res[item] = null;
+  });
+
+  return Object.keys(res);
+  */
+
+  /* --- Second Attempt --- */
+  const res = [];
+
+  arr.forEach((item) => {
+    if (!res.includes(item)) {
+      res.push(item);
+    }
+  });
+
+  return res;
 }
