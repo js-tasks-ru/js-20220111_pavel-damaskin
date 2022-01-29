a7cc9cd965ff2bcbf3bd26a6469fe3eb8edccc15
Damaskin Pavel <falard.damaskin@gmail.com>
2022-01-29 20:43:11 +0200

    Task 5.1 is ready

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,61 @@
 export default class NotificationMessage {
+  static previousNotification = null;
 
+  constructor(message,
+    {
+      duration = 1000,
+      type = '',
+    } = {})
+  {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value: ${this.duration / 1000 + 's'}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type || 'Notification'}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render(notification) {
+    if (this.constructor.previousNotification) {
+      this.constructor.previousNotification.remove();
+    }
+
+    const tmpElem = document.createElement('div');
+    tmpElem.innerHTML = this.template;
+
+    this.element = notification ? notification : tmpElem.firstElementChild;
+
+    document.body.append(this.element);
+
+    this.constructor.previousNotification = this.element;
+
+    setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  }
+
+  show(notification) {
+    this.render(notification);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
