990b7c65fb81cc57ca9e0d2f04e86f1ef2a10d5c
Damaskin Pavel <falard.damaskin@gmail.com>
2022-02-01 15:35:22 +0200

    Task 6.2 is ready

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,83 @@
 class Tooltip {
-  initialize () {
+  static instance = null;
 
+  content = null;
+
+  constructor() {
+    if (!this.constructor.instance) {
+      this.constructor.instance = this;
+    }
+
+    return this.constructor.instance;
+  }
+
+  initialize() {
+    this.attachEvents();
+  }
+
+  render() {
+    const tmpElem = document.createElement('div');
+
+    tmpElem.innerHTML = this.template;
+
+    this.element = tmpElem.firstElementChild;
+
+    document.body.append(this.element);
+  }
+
+  get template() {
+    return `
+      <div class="tooltip">${this.content}</div>
+    `;
+  }
+
+  tooltipHandler({ target }) {
+    const tooltipTrigger = target.closest('[data-tooltip]');
+
+    if (!tooltipTrigger) {
+      return;
+    }
+
+    this.content = tooltipTrigger.dataset.tooltip;
+    this.render();
+
+    this.setTooltipCoords = this.setTooltipCoords.bind(this);
+
+    tooltipTrigger.addEventListener('pointermove', this.setTooltipCoords);
+
+    tooltipTrigger.addEventListener('pointerout', (e) => {
+      this.remove();
+      e.currentTarget.removeEventListener('pointermove', this.setTooltipCoords);
+    }, {
+      once: true,
+    });
+  }
+
+  setTooltipCoords(e) {
+    const xPointerCoord = e.clientX + 5;
+    const yPointerCoord = e.clientY + 5;
+
+    this.element.style.left = `${xPointerCoord}px`;
+    this.element.style.top = `${yPointerCoord}px`;
+  }
+
+  attachEvents() {
+    this.tooltipHandler = this.tooltipHandler.bind(this);
+
+    document.addEventListener('pointerover', this.tooltipHandler);
+  }
+
+  detachEvents() {
+    document.removeEventListener('pointerover', this.tooltipHandler);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.detachEvents();
   }
 }
 
